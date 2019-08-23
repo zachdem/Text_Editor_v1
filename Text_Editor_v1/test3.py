52d12cedc810a846fc1a91e09001bf6ebe11cd4a
zachdem <51308512+zachdem@users.noreply.github.com>
2019-08-22 22:33:35 -0500

    Add files via upload
    Added open save logic

--- Text_Editor_v1/test3.py
+++ Text_Editor_v1/test3.py
@@ -1,30 +1,54 @@
-from tkinter            import Button, Label, Text, Tk, Toplevel, END
-from tkinter.filedialog import asksaveasfilename
+from tkinter            import Button, Label, Menu, Text, Tk, Toplevel, END
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+
+START = 1.0
 
 
 def save_helper(textbox):
-    filename = asksaveasfilename(defaultextension = "txt")
-    with open(filename, 'w') as f:
-        f.write(textbox.get(1.0, END))
-    print("file saved")
+    filename = asksaveasfilename(filetypes = (("text file", "*.txt",),
+                                                ("all files", "*.*")),
+                                    defaultextension = ".txt",
+                                    title = "Select a file to save to")
+    if filename != "":      # if file selection is cancelled
+        f = open(filename, 'w')
+        f.write(textbox.get(START, END))
+        f.close()
+        print("file saved")
+
+def open_helper(textbox):
+    filename = askopenfilename(filetypes = (("text file", "*.txt",),
+                                                ("all files", "*.*")),
+                                    defaultextension = ".txt",
+                                    title = "Select a file to open")
+    if filename != "":      # if file selection is cancelled
+        f = open(filename, 'r')
+        text = f.read()
+        textbox.delete(START, END)
+        textbox.insert(START, text)
+        f.close()
 
 def main():
     root = Tk()
     root.title("ZACH_CHAD_EDITOR")
 
-    side_window = Toplevel(root)
-
     label = Label(root, text = "here Zach")
     label.pack()
 
-    textbox = Text(root, width = 80, height = 40)
+    textbox = Text(root, width = 80, height = 40, undo = True)
     textbox.pack()
 
-    button = Button(root,
-                    text = "save",
-                    command = lambda: save_helper(textbox)
-                    )
-    button.pack()
+    menubar = Menu(root)
+    root["menu"] = menubar
+
+    file_menu = Menu(menubar)
+    menubar.add_cascade(label = "File", menu = file_menu)
+
+    file_menu.add_command(label = "Save",
+                            command = lambda: save_helper(textbox))
+
+    file_menu.add_command(label = "Open",
+                            command = lambda: open_helper(textbox))
 
     root.mainloop()
 
